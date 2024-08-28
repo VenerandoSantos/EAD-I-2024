55968d22c101c785803b222675d28a903fac5b54
VenerandoSantos <162456400+VenerandoSantos@users.noreply.github.com>
2024-08-28 00:04:56 -0300

    Create HelloWord!.c

---
+++ HelloWord!.c
@@ -0,0 +1,17 @@
+/*
+O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+
+Entrada
+Este problema não possui nenhuma entrada.
+
+Saída
+Você deve imprimir a mensagem "Hello World!" e em seguida o final de linha, conforme o exemplo abaixo.
+*/
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    printf("Hello World!\n");
+    return 0;
+}
